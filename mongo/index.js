46c38ca48c53bbc2595668f1ae4097691bb2f5c2
sugallo_sd2023@CEACCS.NET <sugallo_sd2023@CEACCS.NET>
2019-10-03 17:29:41 +0800

    Initial

--- mongo/index.js
+++ mongo/index.js
@@ -1,22 +1,62 @@
-// require('./db/connect');
+require('./db/connect');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost:27017/lorly';
+var db = mongoose.connection;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
 app.set('views', __dirname + '/view');
-// app.use(bodyParser.json());
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 app.use(express.static('public'));
 
-// app.use('/', itemRoutes);
 
-app.get('/item/create', function (req, res) {
-      res.sendFile(__dirname+"/view/index.html");
-  });
+app.post('/item/create', function (req, res) {
+    console.log("recieved!")
+    console.log(req.body);
+    var item = req.body.item;
+        var quantity = req.body.quantity;
+        var pty = req.body.pty;
 
-app.use('*', function(req, res) {
-    res.status(404).json({ message: 'Not Found' });
+        db.collection('hurts').insertOne({ item: item, quantity: quantity, pty: pty }, function () {
+            if (err) throw err;
+            db.close();
+        })
+    res.send(req.body);
 });
 
-app.listen(8080, function() {
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/view/index.html");
+    db.createCollection("hurts", function (err, result) {
+        if (err) throw err;
+        console.log('Collection created')
+        db.close();
+    })
+})
+
+
+
+app.get('/retrieve/all', function (req, res) {
+    db.collection('customer').find({}).toArray(function (err, docs) {
+        if (err) throw err;
+        res.send(docs)
+        db.close();
+    })
+
+})
+
+
+app.listen(8080, function () {
     console.log('Listening on port 8080');
 });
 
